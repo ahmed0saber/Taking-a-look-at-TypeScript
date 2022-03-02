346d1e688507d2c8c4c53d6959914e323fd3d297
Ahmed Saber <ahmed0saber33@gmail.com>
2022-03-03 00:30:38 +0200

    Add files via upload

---
+++ objects/index.js
@@ -0,0 +1,13 @@
+var person = {
+    name: "Ahmed",
+    age: 21,
+    male: true
+};
+console.log(person);
+console.log('----------');
+var banana = {
+    name: "Banana",
+    sweet: true,
+    price: 10
+};
+console.log(banana);
---
+++ objects/index.ts
@@ -0,0 +1,24 @@
+let person: {
+    name: string,
+    age: number,
+    male: boolean
+} = {
+    name: "Ahmed",
+    age: 21,
+    male: true
+}
+console.log(person)
+
+console.log('----------')
+
+type foodType = {
+    name: string,
+    sweet: boolean,
+    price: number
+}
+let banana:foodType = {
+    name: "Banana",
+    sweet: true,
+    price: 10
+}
+console.log(banana)
