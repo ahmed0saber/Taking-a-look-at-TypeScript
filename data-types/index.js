6527c74a3fe49f8b7d8133108e05de1460e6dfd5
Ahmed Saber <ahmed0saber33@gmail.com>
2022-03-02 23:31:28 +0200

    Add files via upload

---
+++ data-types/index.js
@@ -0,0 +1,12 @@
+var x = 5;
+var username = "Ahmed";
+var male = true;
+var myVar = 21;
+console.log(x, username, male, myVar);
+myVar = "ahmed0saber";
+console.log(myVar);
+myVar = {
+    name: 'Ahmed',
+    age: '21'
+};
+console.log(myVar);
---
+++ data-types/index.ts
@@ -0,0 +1,12 @@
+let x:number = 5;
+let username:string = "Ahmed";
+let male:boolean = true;
+let myVar:any = 21
+console.log(x, username, male, myVar)
+myVar = "ahmed0saber"
+console.log(myVar)
+myVar = {
+    name: 'Ahmed',
+    age: '21'
+}
+console.log(myVar)
