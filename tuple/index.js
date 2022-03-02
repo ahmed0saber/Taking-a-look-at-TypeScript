3ec36acd6eb9869b99faff2152d7fdb8d280cc24
Ahmed Saber <ahmed0saber33@gmail.com>
2022-03-02 23:37:55 +0200

    Add files via upload

---
+++ tuple/index.js
@@ -0,0 +1,4 @@
+var data = [15, 24, 32, 49, 75];
+console.log(data);
+var userData = [1, "Ahmed Saber", 21, true];
+console.log(userData);
---
+++ tuple/index.ts
@@ -0,0 +1,5 @@
+let data:number[] = [15, 24, 32, 49, 75]
+console.log(data)
+
+let userData:[number, string, number, boolean] = [1, "Ahmed Saber", 21, true]
+console.log(userData)
